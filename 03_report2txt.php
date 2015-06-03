3942bef9b04d20401f9873ee2515c92d8a2444cd
Finjon Kiang <kiange@gmail.com>
2015-06-03 14:31:31 +0800

    store report files into repo

--- 03_report2txt.php
+++ 03_report2txt.php
@@ -1,28 +1,33 @@
 <?php
+
 $tmpPath = dirname(__FILE__) . '/tmp';
 $txtPath = dirname(__FILE__) . '/report2txt';
+$reportPath = dirname(__FILE__) . '/report';
 
 $errors = array();
 $fh = fopen('reports.csv', 'r');
-while($line = fgetcsv($fh, 2048)) {
+while ($line = fgetcsv($fh, 2048)) {
+    $pathinfo = pathinfo($line[2]);
     $line[1] = str_replace(array('(', ')'), array('_', '_'), $line[1]);
-    $tmpFile = $tmpPath . '/' . md5($line[2]) . '.doc';
+    $reportFile = "{$reportPath}/{$line[1]}.{$pathinfo['extension']}";
     $txtFile = "{$txtPath}/{$line[1]}.txt";
+
     echo "processing {$txtFile}\n";
-    if(!file_exists($tmpFile)) {
-        file_put_contents($tmpFile, file_get_contents($line[2]));
+    if (!file_exists($reportFile)) {
+        file_put_contents($reportFile, file_get_contents($line[2]));
     }
-    if(filesize($tmpFile) === 0) {
+    if (filesize($reportFile) === 0) {
         $errors[] = array(
             'download error',
             implode('|', $line),
         );
+        unlink($reportFile);
         continue;
     }
-    if(!file_exists($txtFile)) {
-        exec("abiword --to=txt {$tmpFile} -o {$txtFile}");
+    if (!file_exists($txtFile)) {
+        exec("abiword --to=txt {$reportFile} -o {$txtFile}");
     }
-    if(filesize($txtFile) === 0) {
+    if (filesize($txtFile) === 0) {
         $errors[] = array(
             'convert error',
             implode('|', $line),
