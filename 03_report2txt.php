7c6eaf89b33cef442acd90a6c1c58daa73d952ec
kiang <kiange@gmail.com>
2016-08-01 15:01:26 +0800

    report files

--- 03_report2txt.php
+++ 03_report2txt.php
@@ -13,7 +13,7 @@ while ($line = fgetcsv($fh, 2048)) {
     $txtFile = "{$txtPath}/{$line[1]}.txt";
 
     echo "processing {$txtFile}\n";
-    if (!file_exists($reportFile)) {
+    if (!file_exists($reportFile) || filesize($reportFile) === 0) {
         file_put_contents($reportFile, file_get_contents($line[2]));
     }
     if (filesize($reportFile) === 0) {
@@ -24,10 +24,11 @@ while ($line = fgetcsv($fh, 2048)) {
         unlink($reportFile);
         continue;
     }
-    if (!file_exists($txtFile)) {
+    if (!file_exists($txtFile) || filesize($txtFile) === 0) {
         exec("abiword --to=txt {$reportFile} -o {$txtFile}");
     }
     if (filesize($txtFile) === 0) {
+      $line[] = $reportFile;
         $errors[] = array(
             'convert error',
             implode('|', $line),
