42251bbc0573b7b496d5fd9bf69b51d364f21524
kiang <kiange@gmail.com>
2016-08-27 16:15:45 +0800

    add highchart

--- main.js
+++ main.js
@@ -20,17 +20,25 @@ function initialize() {
     map.data.addListener('click', function (event) {
         var candidates = event.feature.getProperty('candidates');
         var electionId = event.feature.getProperty('election_id');
-        var content = '<div class="clearfix"></div><h2>' + event.feature.getProperty('election') + '</h2>';
+        var title = event.feature.getProperty('election');
+        var content = '<div class="clearfix"></div><hr /><h2>' + title + '</h2>';
+        var chartCategories = [], incomes = [], outcomes = [];
         content += '<table class="table table-boarded"><thead><tr>';
         for (i = 4; i <= 20; i++) {
             content += '<th>' + header[i] + '</th>';
         }
         content += '</tr></thead><tbody>';
         for (k in candidates) {
+            var income = outcome = 0;
             content += '<tr>';
             for (i = 4; i <= 20; i++) {
                 if (areas[electionId][candidates[k].id]) {
                     content += '<td>' + areas[electionId][candidates[k].id][i] + '</td>';
+                    if (i > 4 && i < 11) {
+                        income += parseInt(areas[electionId][candidates[k].id][i]);
+                    } else if (i > 10) {
+                        outcome += parseInt(areas[electionId][candidates[k].id][i]);
+                    }
                 } else {
                     if (i === 4) {
                         content += '<td>' + candidates[k].name + '</td>';
@@ -40,6 +48,11 @@ function initialize() {
                 }
             }
             content += '</tr>';
+            if (areas[electionId][candidates[k].id]) {
+                chartCategories.push(candidates[k].name);
+                incomes.push(income);
+                outcomes.push(outcome);
+            }
         }
         content += '</tbody></table>';
 
@@ -55,6 +68,63 @@ function initialize() {
         });
 
         $('#areaDetail').html(content);
+
+        $('#chartContainer').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: title
+            },
+            xAxis: {
+                categories: chartCategories
+            },
+            yAxis: [{
+                    min: 0,
+                    title: {
+                        text: '收入'
+                    }
+                }, {
+                    title: {
+                        text: '支出'
+                    },
+                    opposite: true
+                }],
+            legend: {
+                shadow: false
+            },
+            tooltip: {
+                shared: true
+            },
+            plotOptions: {
+                column: {
+                    grouping: false,
+                    shadow: false,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                    name: '收入',
+                    color: 'rgba(248,161,63,1)',
+                    data: incomes,
+                    tooltip: {
+                        valuePrefix: '$'
+                    },
+                    pointPadding: 0.3,
+                    pointPlacement: 0.2,
+                    yAxis: 1
+                }, {
+                    name: '支出',
+                    color: 'rgba(186,60,61,.9)',
+                    data: outcomes,
+                    tooltip: {
+                        valuePrefix: '$'
+                    },
+                    pointPadding: 0.4,
+                    pointPlacement: 0.2,
+                    yAxis: 1
+                }]
+        });
     });
     map.data.addListener('mouseout', function (event) {
         $('#content').html('在地圖上滑動或點選以顯示數據').addClass('text-muted');
@@ -74,7 +144,7 @@ function initialize() {
             map.data.loadGeoJson('json/' + k + '.json');
         }
     });
-    
+
     map.data.setStyle(function (feature) {
         var color = feature.getProperty('color');
         if (color === '') {
