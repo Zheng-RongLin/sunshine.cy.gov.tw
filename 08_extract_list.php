92991a97c69244f9a9515762688221c2cbceb18f
kiang <kiange@gmail.com>
2018-07-18 00:34:19 +0800

    update 2018 list

--- 08_extract_list.php
+++ 08_extract_list.php
@@ -3,6 +3,9 @@ $fh = fopen(__DIR__ . '/list_2018.csv', 'r');
 $result = array();
 while($line = fgetcsv($fh, 2048)) {
   $parts = preg_split('/(年|擬參選人|政治獻金專戶)/', $line[1]);
+  if(!isset($parts[2])) {
+    continue;
+  }
   $city = mb_substr($parts[1], 0, 3, 'utf-8');
   $position = mb_substr($parts[1], 3, null, 'utf-8');
   if(!isset($result[$position])) {
@@ -15,10 +18,12 @@ while($line = fgetcsv($fh, 2048)) {
 }
 
 foreach($result AS $p => $d) {
-  echo "{$p}\n\n";
-  foreach($d AS $city => $names) {
-    $count = count($names);
-    echo "[{$city} - {$count}]\n";
-    echo implode(', ', $names) . "\n";
+  if($p === '市長' || $p === '縣長') {
+    echo "{$p}\n\n";
+    foreach($d AS $city => $names) {
+      $count = count($names);
+      echo "[{$city} - {$count}]\n";
+      echo implode(', ', $names) . "\n";
+    }
   }
 }
